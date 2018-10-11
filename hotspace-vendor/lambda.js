dc40dbbf26935e2f7de418f44a56869900c520e2
TestAndunR <andunranmal@gmail.com>
2018-10-11 10:54:47 +0530

    changes in the ddb

--- hotspace-vendor/lambda.js
+++ hotspace-vendor/lambda.js
@@ -1,13 +1,19 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
-let vendorName = event.vendorName;
-let promo = event.promo;
 exports.handler = function (event, context, callback) {
+    let vendorName = event.vendorName;
+    let promo = event.promoCode;
+    let promoType = event.type;
+
     console.log(vendorName, promo)
     ddb.put({
         TableName: 'hotspacesPromotions',
-        Item: { 'promoCode': 'code', 'VendorName': 'vendorName', 'promotionType': 'promoType' }
+        Item: {
+            'promoCode': promo,
+            'VendorName': vendorName,
+            'promotionType': promoType
+        }
     }).promise().then(function (data) {
         console.log(data)
         //your logic goes here
